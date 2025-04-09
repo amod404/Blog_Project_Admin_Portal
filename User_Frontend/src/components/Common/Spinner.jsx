245c5320df38300408aa22b61c9bfe5c46eb45f5
Amod <amodschool1234@gmail.com>
2025-04-09 16:20:17 +0530

    cors fixed

--- User_Frontend/src/components/Common/Spinner.jsx
+++ User_Frontend/src/components/Common/Spinner.jsx
@@ -3,7 +3,7 @@ import { BeatLoader } from "react-spinners"
 
 const Spinner = () => {
   return(
-    <div className=" bg-whiteflex fixed w-screen h-screen top-0 left-0 items-center justify-center z-50">
+    <div className=" bg-white flex fixed w-screen h-screen top-0 left-0 items-center justify-center z-50">
         <BeatLoader />
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -10,21 +10,20 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001','https://blog-project-admin-portal-g7ji-8sw7rhztm-amod-yadavs-projects.vercel.app'];
 
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     credentials: true // If you're sending cookies or authentication headers
-// }));
-
 app.use(cors({
-    origin: "*", // Allows all origins (only for debugging)
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true // If you're sending cookies or authentication headers
 }));
+// app.use(cors({
+//     origin: "*", // Allows all origins (only for debugging)
+//     credentials: true
+// }));
 
 app.use(
     fileUpload({
